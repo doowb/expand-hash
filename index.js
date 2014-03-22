adb6dcaf16616953e587d97e9a12ecaccb21efec
Brian Woodward <brian.woodward@gmail.com>
2014-03-22 16:38:57 -0400

    adding tests and functionality

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = function () {
-  return 'Hello, world';
+
+module.exports = function (hash) {
+
+  var obj = {};
+
+  for (var key in hash) {
+    if (hash.hasOwnProperty(key)) {
+      if (key.indexOf('.') === -1) {
+        obj[key] = hash[key];
+      } else {
+        var paths = key.split('.');
+        obj[paths[0]] = namespace(obj, key, hash[key]);
+      }
+    }
+  }
+  return obj;
 };
+
+function namespace (obj, path, value) {
+  if (path.indexOf('.') === -1) {
+    obj[path] = value;
+    return obj;
+  }
+  var paths = path.split('.');
+  var key = paths.shift();
+  return namespace((obj[key] = obj[key] || {}), paths.join('.'), value);
+}
--- test/index.js
+++ test/index.js
@@ -2,8 +2,43 @@ var expect = require('chai').expect,
     expandHash = require('..');
 
 describe('expand-hash', function() {
-  it('should say hello', function(done) {
-    expect(expandHash()).to.equal('Hello, world');
+
+  /***
+  var expandHash = require('expand-hash');
+  var hash = {
+    'foo': 'bar',
+    'baz': 'bang'
+  };
+
+  var expanded = expandHash(hash);
+  console.log('expanded: ', expanded);
+  ***/
+
+  it('expand simple hash', function(done) {
+    var simple = { 'foo': 'bar', 'baz': 'bang' };
+    var expected = { foo: 'bar', baz: 'bang' };
+    var actual = expandHash(simple);
+    expect(actual).to.eql(expected);
     done();
   });
+
+  /***
+  var expandHash = require('expand-hash');
+  var hash = {
+    'foo.bar': 'bar',
+    'foo.baz': 'baz',
+    'something': 'else'
+  };
+  var expanded = expandHash(hash);
+  console.log('expanded: ', expanded);
+  **/
+
+  it('expand complex hash', function (done) {
+    var complex = { 'foo.bar': 'bar', 'foo.baz': 'baz', 'beep.boop': 'bop', 'something': 'else' };
+    var expected = { foo: { bar: 'bar', baz: 'baz' }, beep: { boop: 'bop' }, something: 'else' };
+    var actual = expandHash(complex);
+    expect(actual).to.eql(expected);
+    done();
+  });
+
 });
